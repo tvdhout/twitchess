73fbb5c7067dd9696b1274a18780e553a53c3469
Thijs van den Hout <thijsvandenhout@live.nl>
2021-10-02 18:26:39 +0200

    static content injection

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,42 +1,5 @@
 // background.js
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Check if tab is loaded completely, and if the domain is lichess.org
-    // Inject CSS / javascript on loading lichess
-    if (changeInfo.status === 'complete' && /^https?:\/\/(?:\w*\.)?lichess\.org(?!\/api(\/|#|\?|$))/.test(tab.url)) {
-        chrome.scripting.insertCSS({
-            target: {tabId: tabId},
-            files: ['foreground.css']
-        })
-            .then(() => {
-                chrome.scripting.executeScript({
-                    target: {tabId: tabId},
-                    files: ['foreground.js']
-                }, null)
-            })
-            .catch(error => {
-                console.log(error)
-            });
-    } else if (changeInfo.status === 'complete' &&
-        /^https?:\/\/(\w*\.)?twitchess\.app\/setup/.test(tab.url)) {
-        // Set user and token
-        try {
-            const urlParams = new URLSearchParams(tab.url.match(/\?(.*)/)[0]);
-            const user = urlParams.get('user');
-            const token = urlParams.get('token');
-            if (user != null && token != null) {
-                chrome.storage.sync.set({sessionToken: token}, null);
-                chrome.storage.sync.set({user: user}, null);
-            }
-        } catch (TypeError) {
-        }
-        chrome.scripting.executeScript({
-            target: {tabId: tabId},
-            files: ['twitchess.js']
-        }, null)
-    }
-});
-
 function checkConnection(sendResponse) {
     /**
      * @param sendResponse  {response} function to send a response to the message sent by the popup
@@ -69,7 +32,7 @@ function checkConnection(sendResponse) {
     })
 }
 
-function retrieveSubs(sendResponse){
+function retrieveSubsFromAPI(sendResponse){
     chrome.storage.sync.get(['user', 'sessionToken'], data => {
         if (chrome.runtime.lastError) {
             sendResponse({
@@ -104,16 +67,16 @@ function getSubs(sendResponse) {
      * Get a recent list of subs from storage (20 sec), otherwise retrieve them from the API.
      */
     chrome.storage.sync.get(['subs', 'subsDate'], data => {
-        if (chrome.runtime.lastError) {
-            retrieveSubs(sendResponse);
+        if (chrome.runtime.lastError) {  // Data not found in storage
+            retrieveSubsFromAPI(sendResponse);
         } else {
             if (new Date(data.subsDate).getTime() > ((new Date()).getTime() - 10000)) {
                 sendResponse({
                     'success': true,
                     'data': {'subs': data.subs}
                 });
-            } else {
-                retrieveSubs(sendResponse);
+            } else {  // Data in storage older than 10 seconds
+                retrieveSubsFromAPI(sendResponse);
             }
         }
     });
--- chrome_extension/twitchess.js
+++ chrome_extension/twitchess.js
@@ -1,5 +1,16 @@
 document.getElementById('install-extension').innerHTML = '<strike>Install the Twitchess Chrome extension</strike> <span style="color:#ffab61;"><i class="fas fa-check"></i> Installed</span>'
 
+try {
+    const urlParams = new URLSearchParams(location.href.match(/\?(.*)/)[0]);
+    const user = urlParams.get('user');
+    const token = urlParams.get('token');
+    if (user != null && token != null) {
+        chrome.storage.sync.set({sessionToken: token}, null);
+        chrome.storage.sync.set({user: user}, null);
+    }
+} catch (TypeError) {
+}
+
 chrome.runtime.sendMessage({'message': 'check_connected'}, response => {
     if(response.valid){
         document.getElementById('twitch-button-text').innerHTML = 'Generate new token';
